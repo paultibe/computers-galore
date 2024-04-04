bce4acf297d31973b5d3e23e7db716497f67c46c
Paul Tiberghien <r1e6f@ugrad.cs.ubc.ca>
2024-04-04 16:52:04 -0700

    created dummy button + endpoint

--- src/frontend/src/pages/aggregate.tsx
+++ src/frontend/src/pages/aggregate.tsx
@@ -1,6 +1,32 @@
-// stuff 
+import React, { useState } from "react";
 function Aggregate() {
-  return <div>Aggregate </div>;
+  // reactive value + setter
+  const [data, setData] = useState(null);
+
+  // basic fetch
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/getAggregation");
+      console.log("Raw Response:", response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      console.log("Parsed Result:", result);
+      setData(result); // store result in data variable
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setData(null);
+    }
+  };
+
+  return (
+    <div>
+      <div>Aggregate</div>
+      <button onClick={fetchData}>Fetch Data</button>
+      {data && <div>{JSON.stringify(data)}</div>}
+    </div>
+  );
 }
 
 export default Aggregate;
