035a11905394e7d71823438ea0fda7626ffdbd6a
Paul Tiberghien <r1e6f@ugrad.cs.ubc.ca>
2024-04-05 20:50:57 -0700

    small fix

--- src/frontend/src/constants.ts
+++ src/frontend/src/constants.ts
@@ -1,5 +1,5 @@
 // export const BE_BASE_URL: string = 'http://localhost:8000';
 
-export const BE_BASE_URL: string = "http://127.0.0.1:8000";
+// export const BE_BASE_URL: string = "http://127.0.0.1:8000";
 
-// export const BE_BASE_URL: string = "http://192.9.242.103:8000";
+export const BE_BASE_URL: string = "http://192.9.242.103:8000";
